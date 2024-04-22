8300ad5163b961e85629577055973770e5d49ee0
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2024-04-22 14:31:00 -0300

    feat: remove devcontainer plugin

--- init.lua
+++ init.lua
@@ -863,7 +863,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   require 'custom.plugins.omnisharp_vim',
   require 'custom.plugins.schemastore_nvim',
-  require 'custom.plugins.nvim_dev_container',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
